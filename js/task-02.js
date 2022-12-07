873ee9779706d57374c6b08673516bea46fab7c6
Milosska <110258611+Milosska@users.noreply.github.com>
2022-12-08 00:18:48 +0200

    task-2 done

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,23 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+function createIngridientListEl(name) {
+  const liEl = document.createElement("li");
+  liEl.classList.add("item");
+  liEl.textContent = name;
+  return liEl;
+}
+
+const ingridientsList = [];
+ingredients.forEach((elem) =>
+  ingridientsList.push(createIngridientListEl(elem))
+);
+
+const ulEl = document.querySelector("#ingredients");
+ulEl.append(...ingridientsList);
