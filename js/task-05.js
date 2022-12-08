2ca88c07ef1ca27b7ba89db271c46c51dcacef4a
Milosska <110258611+Milosska@users.noreply.github.com>
2022-12-08 11:37:50 +0200

    task-5 done

---
+++ js/task-05.js
@@ -0,0 +1,8 @@
+const inputEl = document.querySelector("#name-input");
+const spanEl = document.querySelector("#name-output");
+
+inputEl.addEventListener("input", () =>
+  inputEl.value
+    ? (spanEl.textContent = inputEl.value)
+    : (spanEl.textContent = "Anonymous")
+);
