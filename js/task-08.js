a1f57f3fe5362866f5c5cc7a1bfb9b83d987a513
Milosska <110258611+Milosska@users.noreply.github.com>
2022-12-09 14:53:59 +0200

    task 8 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,34 @@
+const loginFormEl = document.querySelector(".login-form");
 
+console.dir(loginFormEl);
+console.dir();
+
+loginFormEl.addEventListener("submit", validateSubmit);
+
+function validateSubmit(event) {
+  event.preventDefault();
+  if (
+    !event.currentTarget.elements.email.value ||
+    !event.currentTarget.elements.password.value
+  ) {
+    return alert("All imputs should be filled!");
+  }
+  // Перевірка пошти
+  else if (
+    !event.currentTarget.elements.email.value.includes("@") ||
+    !event.currentTarget.elements.email.value.includes(".")
+  ) {
+    return alert("Invalide email!");
+  } else {
+    const formData = {
+      email: event.currentTarget.elements.email.value,
+      password: event.currentTarget.elements.password.value,
+    };
+
+    // Або:
+    // const formData = new FormData(event.currentTarget);
+
+    console.dir(formData);
+    event.currentTarget.reset();
+  }
+}
