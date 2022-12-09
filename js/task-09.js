ae6ba3ac03df2bb6be43c2d3d1e51306f4ee7ee6
Milosska <110258611+Milosska@users.noreply.github.com>
2022-12-09 15:05:38 +0200

    task-9 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColorBtnEl = document.querySelector(".change-color");
+const colorNameEl = document.querySelector(".color");
+const bodyEl = document.querySelector("body");
+
+changeColorBtnEl.addEventListener("click", () => {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  colorNameEl.textContent = `${bodyEl.style.backgroundColor}`;
+});
